b93ba71837f3fa085a9235ef2095e8c9df82a1e5
hmora <312296414@ciencias.unam.mx>
2020-05-24 14:59:13 -0500

    Generación de CGR en lote en proceso

--- scripts/CGRfuncs.py
+++ scripts/CGRfuncs.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import os, sys
 
 def seq_generator(pathtofasta):
     '''
@@ -44,10 +45,10 @@ def generatePoints(dnaSeq):
 # end
 
 def to_cgr(pathtofasta, figname):
-    '''
+    """
     Obtiene una secuencia de un archivo fasta, obtiene sus puntos y grafica
     su CGR
-    '''
+    """
     pts = generatePoints(seq_generator(pathtofasta))
     #
     x,y = zip(*pts)
@@ -56,4 +57,29 @@ def to_cgr(pathtofasta, figname):
     plt.axis('off')
     plt.tight_layout()
     fig.savefig('figures/'+figname+'.png')
+# end
+
+def batch_cgr(opt):
+    """
+    A partir de la opción dada genera todos las CGR de los archivos
+    correspondientes
+    0 -> betacoronavirus
+    1 -> alphacoronavirus
+    2 -> gammacoronavirus
+    3 -> deltacoronavirus
+    """
+    opts = {0: ('data/raw_data/beta/.', 'beta'),
+    1: ('data/raw_data/alpha/.', 'alpha'),
+    2: ('data/raw_data/gamma/.', 'gamma'),
+    3: ('data/raw_data/delta/.', 'delta')}
+    #
+    current_path = (opts[opt])[0]
+    current_label = (opts[opt])[1]
+    i = 0
+    for file in os.listdir(current_path):
+        current_file = os.path.join(current_path, file)
+        if os.path.isfile(current_file):
+            label = current_label+str(i)
+            i+=1
+            to_cgr(current_file, label)
 # end
--- scripts/Main.py
+++ scripts/Main.py
@@ -1,25 +1,27 @@
-from CGRfuncs import to_cgr
+from CGRfuncs import to_cgr, batch_cgr
 
-'''
+"""
 Clase principal
-'''
+"""
 
-'''
+"""
 Para generar una CGR únicamente hay que proporcionar la ruta del archivo
 y el nombre de salida
-'''
+"""
 
 # Genera test1.png en figures/
-to_cgr('data/raw_data/bat.fasta', 'test1')
+# Ej.
+# to_cgr('data/raw_data/beta/sequences.fasta', 'test1')
 
-# Genera test2.png en figures/
-to_cgr('data/raw_data/sequence.fasta', 'test2')
-
-# Genera test3.png en figures/
-to_cgr('data/raw_data/white-eye.fasta', 'test3')
-
-
-'''
-La idea es poner todos los archivos .fasta en data/ y automatizar la generación
-de sus CGR
-'''
+"""
+Ahora se puede obtener el cgr de todos los .fasta de un directorio con los
+parámetros correspondientes
+0 -> betacoronavirus # Probado
+1 -> alphacoronavirus
+2 -> gammacoronavirus
+3 -> deltacoronavirus
+"""
+batch_cgr(0)
+# batch_cgr(1) # aún no hay data
+# batch_cgr(2) # aún no hay data
+# batch_cgr(3) # aún no hay data
