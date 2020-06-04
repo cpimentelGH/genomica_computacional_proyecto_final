7ac0a09ce42eac1f4fff460d072413bcadf502ac
hmora <312296414@ciencias.unam.mx>
2020-06-04 13:46:42 -0500

    Manejador de .fastas complejos

--- scripts/Test.py
+++ scripts/Test.py
@@ -1,4 +1,5 @@
 from CGRfuncs import seq_generator, to_cgr, batch_cgr
+from CGRfuncs2 import complexPointGen, complexCGRGen
 
 """
 seq_generator() test
@@ -12,8 +13,19 @@ to_cgr() test
 # to_cgr('data/raw_data/beta/sequences.fasta', 'figures/beta/', 'beta0')
 
 """
+foo() test
+"""
+# print(foo('data/raw_data/sequences.fasta'))
+
+"""
+foo_cgr() test
+"""
+complexCGRGen('data/raw_data/sequences1.fasta', 'data/filtered/', 'delta')
+# foo_cgr('data/raw_data/sequences.fasta', 'data/filtered/delta/train', 'delta')
+
+"""
 batch_cgr() test
 """
-batch_cgr('data/raw_data/alpha/','figures/alpha/', 'alpha')
-batch_cgr('data/raw_data/beta/','figures/beta/', 'beta')
-batch_cgr('data/raw_data/gamma/','figures/gamma/', 'beta') # errors catched
+# batch_cgr('data/raw_data/alpha/','figures/alpha/', 'alpha')
+# batch_cgr('data/raw_data/beta/','figures/beta/', 'beta')
+# batch_cgr('data/raw_data/gamma/','figures/gamma/', 'beta') # errors catched
