3001c02677f7ac45ca80e6ee4dfd0f515bb9e696
hmora <312296414@ciencias.unam.mx>
2020-06-07 16:29:31 -0500

    Model modificado

--- scripts/Model.py
+++ scripts/Model.py
@@ -3,6 +3,7 @@ from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D
 from keras.layers import Activation, Dropout, Flatten, Dense
 from keras import backend as K
+import matplotlib.pyplot as plt
 
 # Sorry prof me volé su función del notebook ==================================
 def graf_entrenamiento(historia, archivo):
