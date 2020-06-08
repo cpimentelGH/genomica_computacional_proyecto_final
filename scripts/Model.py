11e7427e50867d1a9148dc2e509e4e2db32a9c92
hmora <312296414@ciencias.unam.mx>
2020-06-07 20:33:05 -0500

    CNNModel ahora es una clase

--- scripts/Model.py
+++ scripts/Model.py
@@ -6,100 +6,102 @@ from keras import backend as K
 import matplotlib.pyplot as plt
 import os, sys
 
-# Sorry prof me volé su función del notebook ==================================
-def graf_entrenamiento(historia, archivo):
-    fig = plt.figure(figsize=(10,10))
-    # plot loss
-    plt.subplot(211)
-    plt.title('Cross Entropy')
-    plt.plot(historia.history['loss'], color='blue', label='train')
-    plt.plot(historia.history['val_loss'], color='orange', label='test')
-    # plot accuracy
-    plt.subplot(212)
-    plt.title('Accuracy')
-    plt.plot(historia.history['accuracy'], color='blue', label='train')
-    plt.plot(historia.history['val_accuracy'], color='orange', label='test')
-    # save plot to file
-    if not os.path.exists(archivo):
-        os.makedirs(archivo)
-    plt.savefig(archivo + '_plot.png')
-    plt.show()
-    del(fig)
-#============================================================================
 
-img_width, img_height = 200, 200
+class CNNModel:
 
-train_data_dir = 'data/filtered/train'
-validation_data_dir = 'data/filtered/test'
-nb_train_samples = 1596
-nb_validation_samples = 660
-epochs = 10
-batch_size = 10
+    """
+    Clase que define la arquitectura de la CNN a utilizar
+    y utilidades asociadas
+    """
+
+    def __init__(self, img_width, img_height):
+            if K.image_data_format() == 'channels_first':
+                input_shape = (3, img_width, img_height)
+            else:
+                input_shape = (img_width, img_height, 3)
+            #
+            self.model = Sequential()
+            self.model.add(Conv2D(32, (3, 3), input_shape=input_shape))
+            self.model.add(Activation('relu'))
+            self.model.add(MaxPooling2D(pool_size=(2, 2)))
+            #
+            self.model.add(Conv2D(32, (3, 3)))
+            self.model.add(Activation('relu'))
+            self.model.add(MaxPooling2D(pool_size=(2, 2)))
+            #
+            self.model.add(Conv2D(64, (3, 3)))
+            self.model.add(Activation('relu'))
+            self.model.add(MaxPooling2D(pool_size=(2, 2)))
+            #
+            self.model.add(Flatten())
+            self.model.add(Dense(64))
+            self.model.add(Activation('relu'))
+            self.model.add(Dropout(0.5))
+            self.model.add(Dense(4))
+            self.model.add(Activation('softmax'))
+            #
+            self.model.compile(loss='binary_crossentropy',
+                          optimizer='adam',
+                          metrics=['accuracy'])
+    # end def
+
+    def load_data(self, pathtotrain, pathtotest):
+        train_datagen = ImageDataGenerator(
+            rescale=1./255,
+            shear_range=0.2,
+            zoom_range=0.2,
+            horizontal_flip=True)
+        #
+        test_datagen = ImageDataGenerator(rescale=1./255)
+        #
+        self.train_generator = train_datagen.flow_from_directory(
+            pathtotrain,
+            target_size=(200, 200),
+            batch_size=10,
+            class_mode='categorical')
+        #
+        self.validation_generator = test_datagen.flow_from_directory(
+            pathtotest,
+            target_size=(200, 200),
+            batch_size=10,
+            class_mode='categorical')
+        #
+        self.train_samples = sum(len(files) for _, _, files in os.walk(pathtotrain))
+        self.test_samples = sum(len(files) for _, _, files in os.walk(pathtotest))
+    # end def
+
+    def train(self, epochs, batch_size, savepath, testname):
+        history = self.model.fit_generator(
+            self.train_generator,
+            steps_per_epoch = self.train_samples // batch_size,
+            epochs=epochs,
+            validation_data = self.validation_generator,
+            validation_steps = self.test_samples // batch_size)
+        #
+        if not os.path.exists(savepath):
+            os.makedirs(savepath)
+        self.graf_entrenamiento(history, savepath)
+        self.model.save_weights(savepath + '/' + testname + '.h5')
+    # end def
 
 
-# Construcción del Modelo ===============================
-if K.image_data_format() == 'channels_first':
-    input_shape = (3, img_width, img_height)
-else:
-    input_shape = (img_width, img_height, 3)
+    def graf_entrenamiento(self, historia, archivo):
+        fig = plt.figure(figsize=(10,10))
+        # plot loss
+        plt.subplot(211)
+        plt.title('Cross Entropy')
+        plt.plot(historia.history['loss'], color='blue', label='train')
+        plt.plot(historia.history['val_loss'], color='orange', label='test')
+        # plot accuracy
+        plt.subplot(212)
+        plt.title('Accuracy')
+        plt.plot(historia.history['accuracy'], color='blue', label='train')
+        plt.plot(historia.history['val_accuracy'], color='orange', label='test')
+        # save plot to file
+        plt.savefig(archivo + '/_plot.png')
+        plt.show()
+        del(fig)
+    # end def
 
-model = Sequential()
-model.add(Conv2D(32, (3, 3), input_shape=input_shape))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
 
-model.add(Conv2D(32, (3, 3)))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-
-model.add(Conv2D(64, (3, 3)))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-
-model.add(Flatten())
-model.add(Dense(64))
-model.add(Activation('relu'))
-model.add(Dropout(0.5))
-model.add(Dense(4))
-model.add(Activation('softmax'))
-
-model.summary() # Resumen de la arquitectura
-
-model.compile(loss='binary_crossentropy',
-              optimizer='adam',
-              metrics=['accuracy'])
-#=======================================================
-
-# Inicialización de conjuntos de prueba y entrenamiento=
-train_datagen = ImageDataGenerator(
-    rescale=1./255,
-    shear_range=0.2,
-    zoom_range=0.2,
-    horizontal_flip=True)
-
-test_datagen = ImageDataGenerator(rescale=1./255)
-
-train_generator = train_datagen.flow_from_directory(
-    'data/filtered/train',
-    target_size=(200, 200),
-    batch_size=10,
-    class_mode='categorical')
-
-validation_generator = test_datagen.flow_from_directory(
-    'data/filtered/test',
-    target_size=(200, 200),
-    batch_size=10,
-    class_mode='categorical')
-#=======================================================
-
-# Entrenamiento y evaluación ============================
-history = model.fit_generator(
-    train_generator,
-    steps_per_epoch=nb_train_samples // batch_size,
-    epochs=epochs,
-    validation_data=validation_generator,
-    validation_steps=nb_validation_samples // batch_size)
-
-graf_entrenamiento(history, 'archive/test1/')
-model.save_weights('archive/test1/Test.h5')
-#=======================================================
+# END CNNModel
---
+++ scripts/Test.py
@@ -0,0 +1,7 @@
+from Model import CNNModel
+
+model = CNNModel(200,200)
+
+model.load_data('data/filtered/train', 'data/filtered/test')
+
+model.train(10,5,'archive/test2','test2')
