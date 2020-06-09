8e4795e07aee7743e25a4def6c5e437d9caaf32c
hmora <312296414@ciencias.unam.mx>
2020-06-09 11:48:19 -0500

    In progress ...

--- scripts/Model.py
+++ scripts/Model.py
@@ -15,37 +15,37 @@ class CNNmodel:
     """
 
     def __init__(self, img_width, img_height):
-            """
-            Constructor de la arquitectura de la CNN
-            """
-            if K.image_data_format() == 'channels_first':
-                input_shape = (3, img_width, img_height)
-            else:
-                input_shape = (img_width, img_height, 3)
-            #
-            self.model = Sequential()
-            self.model.add(Conv2D(32, (3, 3), input_shape=input_shape))
-            self.model.add(Activation('relu'))
-            self.model.add(MaxPooling2D(pool_size=(2, 2)))
-            #
-            self.model.add(Conv2D(32, (3, 3)))
-            self.model.add(Activation('relu'))
-            self.model.add(MaxPooling2D(pool_size=(2, 2)))
-            #
-            self.model.add(Conv2D(64, (3, 3)))
-            self.model.add(Activation('relu'))
-            self.model.add(MaxPooling2D(pool_size=(2, 2)))
-            #
-            self.model.add(Flatten())
-            self.model.add(Dense(64))
-            self.model.add(Activation('relu'))
-            self.model.add(Dropout(0.5))
-            self.model.add(Dense(4))
-            self.model.add(Activation('softmax'))
-            #
-            self.model.compile(loss='binary_crossentropy',
-                          optimizer='adam',
-                          metrics=['accuracy'])
+        """
+        Constructor de la arquitectura de la CNN
+        """
+        if K.image_data_format() == 'channels_first':
+            input_shape = (3, img_width, img_height)
+        else:
+            input_shape = (img_width, img_height, 3)
+        #
+        self.model = Sequential()
+        self.model.add(Conv2D(32, (3, 3), input_shape=input_shape))
+        self.model.add(Activation('relu'))
+        self.model.add(MaxPooling2D(pool_size=(2, 2)))
+        #
+        self.model.add(Conv2D(32, (3, 3)))
+        self.model.add(Activation('relu'))
+        self.model.add(MaxPooling2D(pool_size=(2, 2)))
+        #
+        self.model.add(Conv2D(64, (3, 3)))
+        self.model.add(Activation('relu'))
+        self.model.add(MaxPooling2D(pool_size=(2, 2)))
+        #
+        self.model.add(Flatten())
+        self.model.add(Dense(64))
+        self.model.add(Activation('relu'))
+        self.model.add(Dropout(0.5))
+        self.model.add(Dense(4))
+        self.model.add(Activation('softmax'))
+        #
+        self.model.compile(loss='binary_crossentropy',
+                      optimizer='adam',
+                      metrics=['accuracy'])
     # end def
 
     def load_data(self, pathtotrain, pathtotest):
--- scripts/Test.py
+++ scripts/Test.py
@@ -1,5 +1,7 @@
+from Model import CNNmodel
 from CGRfuncs import data_set_gen
 
-
-data_set_gen('data/raw_data/train/alpha_train.fasta',
-             'figures/alpha_training', 'alpha')
+# m = CNNmodel(200, 200)
+#
+# # data_set_gen('data/raw_data/train/alpha_train.fasta',
+# #              'figures/alpha_training', 'alpha')
